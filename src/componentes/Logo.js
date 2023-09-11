047ab2eb8864f6edf517d70d81ae1c2c5db1454e
OscarPOstos <theholderhaizaki@gmail.com>
2023-09-11 18:57:55 +0200

    Proyecto completo

---
+++ src/componentes/Logo.js
@@ -0,0 +1,12 @@
+import freeCodeCampLogo from '../imagenes/freecodecamp-logo.png';
+
+const Logo = () => (
+    <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+      </div>
+);
+
+export default Logo;
